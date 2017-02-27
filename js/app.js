12a0fb7ab0c40ca933bd5434b5a5c3ad26152d2c
inferman <infernal.manowar@gmail.com>
2017-02-27 22:16:37 +0200

    added slider

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-$(document).foundation()
+//$(document).foundation()
+
+$(document).ready(function(){
+    $('.portfolio-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
