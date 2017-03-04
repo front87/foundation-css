131524aa70f156b62fb17dece762b3cacbe00419
Sergey <sergey@swmple.com>
2017-03-04 10:28:16 +0200

    clean version for class work - removed header slider

--- js/app.js
+++ js/app.js
@@ -24,20 +24,4 @@ $(document).ready(function(){
             }
         ]
     });
-    $('.header-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        arrows: false,
-        fade: true,
-        cssEase: 'linear',
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 1170,
-                settings: "unslick"
-            }
-        ]
-    });
 });
