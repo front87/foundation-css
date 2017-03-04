eb1ca0ad3d5bdb59ea8c5b1af836029be41f4df2
Sergey <sergey@swmple.com>
2017-03-04 13:38:23 +0200

    class work

--- js/app.js
+++ js/app.js
@@ -1,27 +1,16 @@
-//$(document).foundation()
+$(document).foundation();
 
 $(document).ready(function(){
     $('.portfolio-slider').slick({
+        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    infinite: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+        autoplay: true
+    });
+    $('.header-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true
     });
 });
